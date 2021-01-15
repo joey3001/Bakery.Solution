12d43f665a0ffff7b1703cfd0d2ff1b7f9f1035a
Joey3001 <josephnero111@gmail.com>
2021-01-15 10:02:29 -0800

    Add BakeryContext model.

---
+++ Bakery/Models/BakeryContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery.Models 
+{
+  public class BakeryContext : DbContext
+  {
+    public DbSet<Treat> Treats { get; set; } 
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+    public BakeryContext(DbContextOptions options) : base(options) { }
+  }
+}
