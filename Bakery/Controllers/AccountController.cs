d8186ba5d2e89f53a3d341dd6c3093883e70f386
Joey3001 <josephnero111@gmail.com>
2021-01-15 12:06:06 -0800

    Add Login routes to Account Controller.

--- Bakery/Controllers/AccountController.cs
+++ Bakery/Controllers/AccountController.cs
@@ -42,5 +42,22 @@ namespace Bakery.Controllers
         return View();
       }
     }
+    public ActionResult Login()
+    {
+      return View();
+    }
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
