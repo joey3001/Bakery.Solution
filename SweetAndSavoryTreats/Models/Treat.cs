6df70ce6563e628ac095f50b678a6bb791e93317
Joey3001 <josephnero111@gmail.com>
2021-01-15 09:07:22 -0800

    Add models for treats & flavors.

---
+++ SweetAndSavoryTreats/Models/Treat.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System; 
+
+namespace SweetAndSavoryTreats.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.Flavors = new HashSet<TreatFlavor>(); 
+    }
+    public int TreatId { get; set; }
+    public string Description { get; set;}
+    public ICollection<TreatFlavor> Flavors { get; set; } 
+  }
+}
